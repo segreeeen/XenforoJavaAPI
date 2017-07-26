58cfdc4a30f30be0af54257cc12333b9aa70c9bb
Thomas Sulzbacher <thomas.sulzbacher@me.com>
2017-07-27 01:09:49 +0200

    + added a custom exception to handle errors if parser returns an empty string

---
+++ src/at/hsol/xenapi/err/ValueNotFoundException.java
@@ -0,0 +1,31 @@
+package at.hsol.xenapi.err;
+
+import at.hsol.xenapi.PostConstants;
+
+/**
+ * Custom exception to throw if a parser was not able to find the given
+ * {@link PostConstants}.
+ * 
+ * @author thomassulzbacher
+ *
+ */
+public class ValueNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4628601126447784098L;
+
+	public ValueNotFoundException() {
+		super();
+	}
+
+	public ValueNotFoundException(String message) {
+		super(message);
+	}
+
+	public ValueNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
